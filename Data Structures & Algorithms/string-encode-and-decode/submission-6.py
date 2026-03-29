24f7ce65c4ac46a12384d1381764568b3e62fa8e
MateusCarlos <mateuscarlos.ti@gmail.com>
2026-03-29 13:32:43 -0300

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,23 @@
+#comprimento-prefixo - TC:O(n) | SC:O(n)
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for s in strs:
+            encoded += str(len(s)) + "#" + s
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        ans = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+
+            length = int(s[i:j])
+            ans.append(s[j+1:j+1+length])
+
+            i = j + 1 + length
+        
+        return ans
